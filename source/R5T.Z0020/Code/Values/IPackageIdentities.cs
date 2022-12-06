3e38385d64166f705ad9b303956a00a563e022f1
davidcoats <david@getsafetycone.com>
2022-12-06 12:39:54 -0800

    Interim changes.

--- source/R5T.Z0020/Code/Values/IPackageIdentities.cs
+++ source/R5T.Z0020/Code/Values/IPackageIdentities.cs
@@ -8,6 +8,7 @@ namespace R5T.Z0020
 	[ValuesMarker]
 	public partial interface IPackageIdentities : IValuesMarker
 	{
+        public string Microsoft_AspNetCore_Components_Web => "Microsoft.AspNetCore.Components.Web";
         public string Microsoft_AspNetCore_Components_WebAssembly => "Microsoft.AspNetCore.Components.WebAssembly";
         public string Microsoft_AspNetCore_Components_WebAssembly_DevServer => "Microsoft.AspNetCore.Components.WebAssembly.DevServer";
         public string Microsoft_AspNetCore_Components_WebAssembly_Server => "Microsoft.AspNetCore.Components.WebAssembly.Server";
--- source/R5T.Z0020/Code/Values/IPackages.cs
+++ source/R5T.Z0020/Code/Values/IPackages.cs
@@ -9,6 +9,13 @@ namespace R5T.Z0020
 	[ValuesMarker]
 	public partial interface IPackages : IValuesMarker
 	{
+        public PackageReference Microsoft_AspNetCore_Components_Web => this.Microsoft_AspNetCore_Components_Web_6_0_11;
+        public PackageReference Microsoft_AspNetCore_Components_Web_6_0_11 => new PackageReference
+        {
+            Identity = PackageIdentities.Instance.Microsoft_AspNetCore_Components_Web,
+            Version = F0000.VersionOperator.Instance.From("6.0.11"),
+        };
+
         public PackageReference Microsoft_AspNetCore_Components_WebAssembly => this.Microsoft_AspNetCore_Components_WebAssembly_6_0_11;
         public PackageReference Microsoft_AspNetCore_Components_WebAssembly_6_0_11 => new PackageReference
         {
@@ -32,6 +39,11 @@ namespace R5T.Z0020
             Version = F0000.VersionOperator.Instance.From("6.0.11"),
         };
 
+
+        public PackageReference[] ForRazorClassLibrary => new[]
+        {
+            this.Microsoft_AspNetCore_Components_Web,
+        };
 
         public PackageReference[] ForWebBlazorServer => new[]
 		{
