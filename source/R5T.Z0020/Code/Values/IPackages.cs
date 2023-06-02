72de2d39711fd2a7b7b22cf6d95e3c6bc012d8c8
davidcoats <david@getsafetycone.com>
2023-06-01 18:08:00 -0700

    Interim changes.

--- source/R5T.Z0020/Code/Values/IPackages.cs
+++ source/R5T.Z0020/Code/Values/IPackages.cs
@@ -6,25 +6,32 @@ using R5T.T0152;
 
 namespace R5T.Z0020
 {
+    [Obsolete("See R5T.Z0050.")]
 	[ValuesMarker]
 	public partial interface IPackages : IValuesMarker
 	{
+        [Obsolete("See R5T.Z0050.")]
         public PackageReference Microsoft_AspNetCore_Components_Web => this.Microsoft_AspNetCore_Components_Web_6_0_11;
+        [Obsolete("See R5T.Z0050.")]
         public PackageReference Microsoft_AspNetCore_Components_Web_6_0_11 => new PackageReference
         {
             Identity = PackageIdentities.Instance.Microsoft_AspNetCore_Components_Web,
             Version = F0000.VersionOperator.Instance.From("6.0.11"),
         };
 
+        [Obsolete("See R5T.Z0050.")]
         public PackageReference Microsoft_AspNetCore_Components_WebAssembly => this.Microsoft_AspNetCore_Components_WebAssembly_6_0_11;
+        [Obsolete("See R5T.Z0050.")]
         public PackageReference Microsoft_AspNetCore_Components_WebAssembly_6_0_11 => new PackageReference
         {
             Identity = PackageIdentities.Instance.Microsoft_AspNetCore_Components_WebAssembly,
             Version = F0000.VersionOperator.Instance.From("6.0.11"),
         };
 
+        [Obsolete("See R5T.Z0050.")]
         public PackageReference Microsoft_AspNetCore_Components_WebAssembly_DevServer => this.Microsoft_AspNetCore_Components_WebAssembly_DevServer_6_0_11;
-		public PackageReference Microsoft_AspNetCore_Components_WebAssembly_DevServer_6_0_11 => new PackageReference
+        [Obsolete("See R5T.Z0050.")]
+        public PackageReference Microsoft_AspNetCore_Components_WebAssembly_DevServer_6_0_11 => new PackageReference
 		{
 			Identity = PackageIdentities.Instance.Microsoft_AspNetCore_Components_WebAssembly_DevServer,
 			Version = F0000.VersionOperator.Instance.From("6.0.11"),
@@ -32,7 +39,9 @@ namespace R5T.Z0020
             PrivateAssets = AssetsTagValues.Instance.All,
 		};
 
+        [Obsolete("See R5T.Z0050.")]
         public PackageReference Microsoft_AspNetCore_Components_WebAssembly_Server => this.Microsoft_AspNetCore_Components_WebAssembly_Server_6_0_11;
+        [Obsolete("See R5T.Z0050.")]
         public PackageReference Microsoft_AspNetCore_Components_WebAssembly_Server_6_0_11 => new PackageReference
         {
             Identity = PackageIdentities.Instance.Microsoft_AspNetCore_Components_WebAssembly_Server,
