2932e48c53b33706aafce7a44b23516b119e74f0
davidcoats <david@getsafetycone.com>
2023-07-10 13:53:31 -0700

    Interim changes.

--- source/R5T.Z0020/Code/ΩInstances/Packages.cs
+++ source/R5T.Z0020/Code/ΩInstances/Packages.cs
@@ -3,11 +3,13 @@ using System;
 
 namespace R5T.Z0020
 {
+	[Obsolete("See R5T.Z0050.")]
 	public class Packages : IPackages
 	{
 		#region Infrastructure
 
 	    public static IPackages Instance { get; } = new Packages();
+
 
 	    private Packages()
 	    {
